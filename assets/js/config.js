b0258a3961708e490e62df8e637ac447d1b06471
Arqane <jeagle426@gmail.com>
2026-03-03 11:21:05 -0600

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,11 +4,11 @@ export const API_BASE =
   "https://script.google.com/macros/s/AKfycbwZ-QIjMGQsmeS_z8WDtzndBcJ5XfsYPPoMsINuVPBAo0zm3TK7rDq4CaRNynTB1Unm/exec";
 
 export const PLANETS = [
-  { id: "test", label: "TEST" },
-  { id: "parallax", label: "Parallax" },
-  { id: "cyqs", label: "Cyq`s" },
-  { id: "sevyr", label: "Sevyr" },
-  { id: "octavium", label: "Octavium" },
+  { id: "test",     label: "TEST",     spreadsheetId: "1TkHl3TI0ADTgnOOykI8AEJY1JxIO1nKgCBroI25cuCQ" },
+  { id: "parallax", label: "Parallax", spreadsheetId: "1JdpKT4ojvIonDcqns0vIiCmY99I4d9FI6M8LoONfWAA" },
+  { id: "cyqs",     label: "Cyq`s",    spreadsheetId: "1A3muixDdn4Z62YxX6YexukNci1E8RzHGu_5z11Csdwg" },
+  { id: "sevyr",    label: "Sevyr",    spreadsheetId: "10KK5Pam7HI6EEY0SGlGQFXxoa0rVJ6eQ4v0YVlP4oJA" },
+  { id: "octavium", label: "Octavium", spreadsheetId: "1v4dM17_x9aWs5bb6TF64JbSvcf3VOcn3fsEnQyTK7_k" },
 ];
 
 // Diplomacy relationship styles
